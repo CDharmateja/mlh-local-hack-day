88fb24401591f9bcc69a6587a15546edf5a82de4
C DHARMATEJA <cdharmateja1999@gmail.com>
2018-12-01 11:28:32 +0530

    feat: books-list

--- src/components/books.js
+++ src/components/books.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
 class Books extends Component {
+  booksHTML() {
+    return (
+      <li>
+        <img src="https://via.placeholder.com/150" alt="Placeholder" />
+        <h2>Books</h2>
+        <p>This is description</p>
+        <button>Details</button>
+      </li>
+    );
+  }
+
   render() {
+    const books = [];
+    for (let i = 0; i < 10; i++) {
+      books.push(this.booksHTML());
+    }
     return (
       <div>
         This is books component
+        <ul>
+          { books }
+        </ul>
       </div>
-    )
+    );
   }
 }
 

7ddf5df922c16dcbe657215bac6fd4313b308000
harish chennupati <y17cs027@rvrjc.ac.in>
2018-12-01 15:26:39 +0530

    feat: landing page

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 
 import { Link, Route } from 'react-router-dom';
 
+import './header.css';
+
 class Header extends Component {
   render() {
     return (
       <header>
-        Header
-        <Link to='/'>Landing</Link>
-        <Link to='/books'>Books</Link>
-      </header>
+      <div className="header-right">
+        <a className="active" href="#">Login</a>
+        <a href="#about">About</a>
+      </div>
+      <h1 className="heading">Book's Store</h1>
+      <Link to='/books' className="all-books">All books</Link>
+    </header>
     )
   }
 }
-
 export default Header;
